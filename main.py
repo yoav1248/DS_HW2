87104aee400438d4dc2e656c8aaa4afaf7c39fad
yoav1248 <yoav.alex1248@gmail.com>
2025-06-18 21:50:40 +0300

    Fixed newline at end of output file

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ print()
 print("Part B: ")
 df = load_data(CSV_PATH)
 data = transform_data(df, ["cnt", "t1"])
-for k in [2, 3, 5]:
+
+ks = [2, 3, 5]
+for i, k in enumerate(ks):
     labels, centroids = kmeans(data, k)
 
     print(f"k = {k}")
     print(np.array_str(centroids, precision=3, suppress_small=True))
-    print()
+    if i != len(ks) - 1:
+        print()
 
     visualize_results(data, labels, centroids, f"plot_{k}{'_sample' if IS_SAMPLE else ''}.{FILE_FORMAT}")
